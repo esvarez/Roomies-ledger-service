f35d43970d2488e9acb996c6f1b0e2ebf0baa565
Erick Suárez <ericksuarexdev@gmail.com>
2020-03-15 02:41:41 -0600

    Initial commit

---
+++ src/main/java/dev/ericksuarez/roomies/ledger/service/RoomiesLedgerServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.ericksuarez.roomies.ledger.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomiesLedgerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomiesLedgerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ericksuarez/roomies/ledger/service/RoomiesLedgerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ericksuarez.roomies.ledger.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomiesLedgerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
